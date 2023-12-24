026d92f4fabae4ef21cb70632c5eb292eca7fee4
Poupouski <105277484+Poupouski@users.noreply.github.com>
2023-12-24 17:06:10 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * @file boat.c
+ * @brief main file where the game is started
+ * @author Hicham Bettahar
+ * @date December 24, 2023
+ */
+#include "header.h"
+
+
+int main(){
+         
+        srand(time(NULL));
+        menu();
+
+        return 0;
+}
